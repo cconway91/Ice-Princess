5ff0bb13df611848e37bc7b53d9016386d54e78d
Colter Conway <colterconway@gmail.com>
2019-02-15 16:58:17 -0700

    Initial Commit

---
+++ Ice Princess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ice Princess
+//
+//  Created by Colter  Conway on 2/15/19.
+//  Copyright © 2019 Colter  Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
